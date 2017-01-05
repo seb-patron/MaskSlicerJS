939efe08330394d48aef121c832231d03c9e663c
Sebastian Patron <sebastian.patrones@gmail.com>
2017-01-04 20:20:42 -0700

    Refactored to be ECMA6 compliant

--- public/js/script.js
+++ public/js/script.js
@@ -4,11 +4,12 @@ mask.src = "./masks/mask2.png";
 background.src = "./images/kanye4.jpg";
 
 
-var canvas = document.createElement("canvas"), ctx = canvas.getContext('2d');
-var target = document.getElementById("target"), targetCtx = target.getContext('2d');
+const canvas = document.createElement("canvas"), ctx = canvas.getContext('2d');
+const target = document.getElementById("target"), targetCtx = target.getContext('2d');
     
 mask.addEventListener('load', function() {
-    var l = (background.width / 2) - (mask.width / 2), t = (background.height / 2) - (mask.height / 2);
+    //gets center so mask is always centerred, even if smaller than image
+    const l = (background.width / 2) - (mask.width / 2), t = (background.height / 2) - (mask.height / 2);
     
     canvas.width = background.width;
     canvas.height = background.height;
@@ -17,6 +18,6 @@ mask.addEventListener('load', function() {
     ctx.globalCompositeOperation = "source-in";
     ctx.drawImage(background, 0, 0);
             
-    var imageData = ctx.getImageData(l, t, mask.width, mask.height);                                       
+    const imageData = ctx.getImageData(l, t, mask.width, mask.height);                                       
     targetCtx.putImageData(imageData, 50, 50);  
 });
