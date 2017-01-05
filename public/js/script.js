5b4208e888932dbd12006a443c019f1afb5fba81
Sebastian Patron <sebastian.patrones@gmail.com>
2017-01-04 20:11:05 -0700

    changed variable names to be more obvious

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,21 @@
-var mask = new Image(), bg = new Image();
+var mask = new Image(), background = new Image();
     
 mask.src = "./masks/mask2.png";
-bg.src = "./images/kanye4.jpg";
+background.src = "./images/kanye4.jpg";
 
 
 var canvas = document.createElement("canvas"), ctx = canvas.getContext('2d');
 var target = document.getElementById("target"), targetCtx = target.getContext('2d');
     
-mask.addEventListener('load', function() {    
-    var l = (bg.width / 2) - (mask.width / 2), t = (bg.height / 2) - (mask.height / 2);
+mask.addEventListener('load', function() {
+    var l = (background.width / 2) - (mask.width / 2), t = (background.height / 2) - (mask.height / 2);
     
-    canvas.width = bg.width;
-    canvas.height = bg.height;
+    canvas.width = background.width;
+    canvas.height = background.height;
     
     ctx.drawImage(mask, l, t);
     ctx.globalCompositeOperation = "source-in";
-    ctx.drawImage(bg, 0, 0);
+    ctx.drawImage(background, 0, 0);
             
     var imageData = ctx.getImageData(l, t, mask.width, mask.height);                                       
     targetCtx.putImageData(imageData, 50, 50);  
